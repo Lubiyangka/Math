228459bb8296d56c42204054333b828ef099cd81
WiserZhou <wiserx@qq.com>
2023-08-25 21:57:13 +0800

    增加了路径显示

--- floyd.py
+++ floyd.py
@@ -1,28 +1,25 @@
 def floyd(graph):
     n = len(graph)  # 节点的数量
-    print(n)
-    arrayL = []
-    for i in range(n):
-        arrayL.append(i+1)
-    formatted_row = [f"{x:3}" if x != float('inf') else "inf" for x in arrayL]
-    print(formatted_row)
 
-
-    # 初始化距离矩阵
+    # 初始化距离矩阵和路径矩阵
     dist = [[float('inf')]*n for _ in range(n)]
+    path = [[-1]*n for _ in range(n)]
     for i in range(n):
         dist[i][i] = 0
     for u, v, w in graph:
         dist[u][v] = w
+        path[u][v] = u
 
-    # 逐步更新距离矩阵
+    # 逐步更新距离矩阵和路径矩阵
     for k in range(n):
         for i in range(n):
             for j in range(n):
                 if dist[i][k] + dist[k][j] < dist[i][j]:
                     dist[i][j] = dist[i][k] + dist[k][j]
+                    path[i][j] = path[k][j]
 
-    return dist
+    return dist, path
+
 
 graph = [(1, 2, 1), (1, 25, 1), (2, 3, 1),(3,4,1),(4,25,1),(4,5,1),(4,24,1),(5,6,1),(24,6,1),(24,26,1),(24,23,1),(6,23,1)
          ,(6,7,1),(7,22,1),(7,8,1),(8,9,1),(9,10,1),(26,27,1),(23,21,1),(27,21,1),(21,20,1),(20,19,1),(18,19,1)
@@ -36,9 +33,20 @@ for i, j, k in graph:
     new_graph.append((j, i, k))
 
 print(new_graph)
-shortest_paths = floyd(new_graph)
+shortest_dists, shortest_paths = floyd(new_graph)
 
-# 输出最短路径距离矩阵
-for row in shortest_paths:
-    formatted_row = [f"{x:3}" if x != float('inf') else "inf" for x in row]
-    print(formatted_row)
+# 输出最短路径距离矩阵和具体路径
+for i in range(len(shortest_dists)):
+    for j in range(len(shortest_dists[i])):
+        print(f"节点 {i} 到节点 {j} 的最短路径距离为 {shortest_dists[i][j]}")
+        if shortest_dists[i][j] != float('inf'):
+            path = []
+            k = j
+            while k != i:
+                path.append(k)
+                k = shortest_paths[i][k]
+            path.append(i)
+            path.reverse()
+            print(f"节点 {i} 到节点 {j} 的最短路径为 {path}")
+        print()
+
