89cc13003c8d47fff841084a1853ed88b9ef9bec
WiserZhou <wiserx@qq.com>
2023-08-26 09:45:43 +0800

    第一关的相关参数上传设置

---
+++ plan.py
@@ -0,0 +1,22 @@
+
+
+# 共三十天，晴朗为1，高温为2，沙暴为3
+day = [2,2,1,3,1,2,3,1,2,2,3,2,1,2,2,2,3,3,2,2,1,1,2,1,3,2,1,1,2,2]
+# 负重上限
+WeightMax = 1200
+# 基础收益
+basicProfit = 1000
+# 初始资金
+beginMoney = 10000
+# 基础消耗量
+basicConsume = {
+    1:(5,7),
+    2:(8,6),
+    3:(10,10)
+}
+# 基准价格
+basicPriceWater = 5
+basicPriceFood = 10
+# 每箱质量
+basicWeightWater = 3
+basicWeightFood = 2
